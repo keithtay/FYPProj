c3e1f3c284a2d892fd11de9fcf3d1485438f2f57
Keith Tay <keithtay1991@gmail.com>
2015-08-24 17:04:32 +0800

    Do up main master design and positioning of icons. Added some simple logic process of capturing names and changing the intent

--- app/src/main/java/com/example/keith/fyp/Login.java
+++ app/src/main/java/com/example/keith/fyp/Login.java
@@ -1,17 +1,26 @@
 package com.example.keith.fyp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class Login extends ActionBarActivity {
-
+    Button button;
+    EditText et1;
+    String Username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton();
     }
 
 
@@ -22,6 +31,25 @@ public class Login extends ActionBarActivity {
         return true;
     }
 
+    public void loginButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.loginButton);
+        et1 = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Username = et1.getText().toString();
+                Toast.makeText(getApplicationContext(),"Welcome back " + Username + "!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(context, Patient.class);
+                startActivity(intent);
+
+            }
+        });
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
