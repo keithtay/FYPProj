8721f8da716a9f88f0f100bc7d572e3453818e79
Keith Tay <keithtay1991@gmail.com>
2015-09-29 15:28:29 +0800

    For filter spinner

---
+++ app/src/main/java/com/example/keith/fyp/models/FilterList.java
@@ -0,0 +1,25 @@
+package com.example.keith.fyp.models;
+
+/**
+ * Created by Keith on 28/9/2015.
+ */
+public class FilterList {
+    private String name;
+private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
