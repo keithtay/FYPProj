d2058c018c2ecbee0a5ad9a71abf0db185d978e0
sutris-sdp <sutris.sdp@gmail.com>
2015-10-05 00:58:25 +0800

    Enable OCR module

--- app/src/main/java/com/example/keith/fyp/views/activities/CreatePatientActivity.java
+++ app/src/main/java/com/example/keith/fyp/views/activities/CreatePatientActivity.java
@@ -6,10 +6,17 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -31,6 +38,8 @@ import com.example.keith.fyp.utils.UtilsUi;
 import com.example.keith.fyp.views.fragments.PatientInfoCategListFragment;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.googlecode.leptonica.android.ReadFile;
+import com.googlecode.tesseract.android.TessBaseAPI;
 import com.melnykov.fab.FloatingActionButton;
 import com.mikepenz.iconics.utils.Utils;
 import com.mikepenz.materialdrawer.Drawer;
@@ -39,9 +48,15 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import org.joda.time.DateTime;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class CreatePatientActivity extends AppCompatActivity implements CreatePatientCommunicator, Drawer.OnDrawerItemClickListener {
@@ -200,7 +215,7 @@ public class CreatePatientActivity extends AppCompatActivity implements CreatePa
         Log.i(TAG, "resultCode: " + resultCode);
 
         if (resultCode == -1) {
-//            onPhotoTaken();
+            onPhotoTaken();
         } else {
             Log.v(TAG, "User cancelled");
         }
@@ -227,8 +242,8 @@ public class CreatePatientActivity extends AppCompatActivity implements CreatePa
                 onBackPressed();
                 return true;
             case R.id.action_ocr:
-//                prepareOcrEnvirontment();
-//                startCaptureImageOcrActivity();
+                prepareOcrEnvirontment();
+                startCaptureImageOcrActivity();
         }
         return true;
     }
@@ -331,145 +346,145 @@ public class CreatePatientActivity extends AppCompatActivity implements CreatePa
         startActivity(intent);
     }
 
-//    private void prepareOcrEnvirontment() {
-//        String lang = Global.LANG;
-//        String DATA_PATH = Global.DATA_PATH;
-//
-//        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
-//
-//        for (String path : paths) {
-//            File dir = new File(path);
-//            if (!dir.exists()) {
-//                if (!dir.mkdirs()) {
-//                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
-//                    return;
-//                } else {
-//                    Log.v(TAG, "Created directory " + path + " on sdcard");
-//                }
-//            }
-//
-//        }
-//
-//        // lang.traineddata file with the app (in assets folder)
-//        // You can get them at:
-//        // http://code.google.com/p/tesseract-ocr/downloads/list
-//        // This area needs work and optimization
-//        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
-//            try {
-//
-//                AssetManager assetManager = getAssets();
-//                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
-//                //GZIPInputStream gin = new GZIPInputStream(in);
-//                OutputStream out = new FileOutputStream(DATA_PATH
-//                        + "tessdata/" + lang + ".traineddata");
-//
-//                // Transfer bytes from in to out
-//                byte[] buf = new byte[1024];
-//                int len;
-//                //while ((lenf = gin.read(buff)) > 0) {
-//                while ((len = in.read(buf)) > 0) {
-//                    out.write(buf, 0, len);
-//                }
-//                in.close();
-//                //gin.close();
-//                out.close();
-//
-//                Log.v(TAG, "Copied " + lang + " traineddata");
-//            } catch (IOException e) {
-//                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
-//            }
-//        }
-//    }
-//
-//    private String path = Global.DATA_PATH + "ocr.jpg";;
-//
-//    private void startCaptureImageOcrActivity() {
-//        File file = new File(path);
-//        Uri outputFileUri = Uri.fromFile(file);
-//
-//        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-//
-//        startActivityForResult(intent, 0);
-//    }
-//
-//    private void onPhotoTaken() {
-//        BitmapFactory.Options options = new BitmapFactory.Options();
-//        options.inSampleSize = 4;
-//
-//        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
-//
-//        try {
-//            ExifInterface exif = new ExifInterface(path);
-//            int exifOrientation = exif.getAttributeInt(
-//                    ExifInterface.TAG_ORIENTATION,
-//                    ExifInterface.ORIENTATION_NORMAL);
-//
-//            Log.v(TAG, "Orient: " + exifOrientation);
-//
-//            int rotate = 0;
-//
-//            switch (exifOrientation) {
-//                case ExifInterface.ORIENTATION_ROTATE_90:
-//                    rotate = 90;
-//                    break;
-//                case ExifInterface.ORIENTATION_ROTATE_180:
-//                    rotate = 180;
-//                    break;
-//                case ExifInterface.ORIENTATION_ROTATE_270:
-//                    rotate = 270;
-//                    break;
-//            }
-//
-//            Log.v(TAG, "Rotation: " + rotate);
-//
-//            if (rotate != 0) {
-//
-//                // Getting width & height of the given image.
-//                int w = bitmap.getWidth();
-//                int h = bitmap.getHeight();
-//
-//                // Setting pre rotate
-//                Matrix mtx = new Matrix();
-//                mtx.preRotate(rotate);
-//
-//                // Rotating Bitmap
-//                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
-//            }
-//
-//            // Convert to ARGB_8888, required by tess
-//            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-//
-//        } catch (IOException e) {
-//            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
-//        }
-//
-//        // _image.setImageBitmap( bitmap );
-//
-//        Log.v(TAG, "Before baseApi");
-//
-//        TessBaseAPI baseApi = new TessBaseAPI();
-//        baseApi.setDebug(true);
-//        baseApi.init(Global.DATA_PATH, Global.LANG);
-//        baseApi.setImage(bitmap);
-//
-//        String recognizedText = baseApi.getUTF8Text();
-//
-//        baseApi.end();
-//
-//        // You now have the text in recognizedText var, you can do anything with it.
-//        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-//        // so that garbage doesn't make it to the display.
-//
-//        Log.v(TAG, "OCRED TEXT: " + recognizedText);
-//
-//
-//        recognizedText = recognizedText.trim();
-//
-//        if ( recognizedText.length() != 0 ) {
-//            Intent intent = new Intent(this, OcrReviewActivity.class);
-//            intent.putExtra(Global.EXTRA_RECOGNIZED_TEXT, recognizedText);
-//            startActivity(intent);
-//        }
-//    }
+    private void prepareOcrEnvirontment() {
+        String lang = Global.LANG;
+        String DATA_PATH = Global.DATA_PATH;
+
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+    }
+
+    private String path = Global.DATA_PATH + "ocr.jpg";;
+
+    private void startCaptureImageOcrActivity() {
+        File file = new File(path);
+        Uri outputFileUri = Uri.fromFile(file);
+
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(intent, 0);
+    }
+
+    private void onPhotoTaken() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
+
+        // _image.setImageBitmap( bitmap );
+
+        Log.v(TAG, "Before baseApi");
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(Global.DATA_PATH, Global.LANG);
+        baseApi.setImage(ReadFile.readBitmap(bitmap));
+
+        String recognizedText = baseApi.getUTF8Text();
+
+        baseApi.end();
+
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
+
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+
+        recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0 ) {
+            Intent intent = new Intent(this, OcrReviewActivity.class);
+            intent.putExtra(Global.EXTRA_RECOGNIZED_TEXT, recognizedText);
+            startActivity(intent);
+        }
+    }
 }
