80b9e731b1d880dda264920a5aad05a584ae3a15
teok0045 <heroes90>
2015-12-19 23:11:38 +0800

    photo album functionalities done. Pls check on actual tablet to see if it can load to camera app (not beyond there). Back button will go back to PEAR app.

--- app/src/main/java/com/example/keith/fyp/views/fragments/ViewPatientPhotoAlbumFragment.java
+++ app/src/main/java/com/example/keith/fyp/views/fragments/ViewPatientPhotoAlbumFragment.java
@@ -1,9 +1,13 @@
 package com.example.keith.fyp.views.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,6 +26,7 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.andexert.expandablelayout.library.ExpandableLayout;
 import com.andexert.expandablelayout.library.ExpandableLayoutListView;
@@ -39,6 +44,8 @@ import com.mikepenz.iconics.utils.Utils;
 
 import org.joda.time.DateTime;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -57,9 +64,16 @@ public class ViewPatientPhotoAlbumFragment extends Fragment {
     private LinearLayout addNewPhotosHeaderContainer;
     private ExpandableLayout addNewPhotosExpandable;
     private Button cancelAddNewPhotosButton;
+    private Button addnewPhotosButton;
     private ListView photoAlbumListView;
     private MaterialSpinner photoAlbumTitleSpinner;
+    private String addPhotosSpinnerText;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
+    //will need to edit these 3 parameters to to save the picture directly into database. current in static.
+    private String fileName = "new_photo.jpg";
+    private String path = Global.DATA_PATH + fileName;
+    private Uri outputFileUri;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -99,10 +113,7 @@ public class ViewPatientPhotoAlbumFragment extends Fragment {
 
 
         ArrayList<Bitmap> others = new ArrayList<>();
-        others.add(BitmapFactory.decodeResource(getResources(), R.drawable.avatar_01));
-        others.add(BitmapFactory.decodeResource(getResources(), R.drawable.avatar_02));
-        others.add(BitmapFactory.decodeResource(getResources(), R.drawable.avatar_03));
-        others.add(BitmapFactory.decodeResource(getResources(), R.drawable.avatar_04));
+
 
         photoAlbumList.add(new PhotoAlbum("Others", others));
 
@@ -117,6 +128,7 @@ public class ViewPatientPhotoAlbumFragment extends Fragment {
 
         /////////////////////////////////////listeners////////////////////////////
 
+        //ontouch listener for "add New Photos" card view. once touched while open, it will clear fields and close it.
         addNewPhotosExpandable = (ExpandableLayout) rootView.findViewById(R.id.add_new_photo_expandable_layout);
         addNewPhotosHeaderContainer = (LinearLayout) rootView.findViewById(R.id.add_new_photos_header);
         addNewPhotosHeaderContainer.setOnTouchListener(new View.OnTouchListener() {
@@ -141,6 +153,16 @@ public class ViewPatientPhotoAlbumFragment extends Fragment {
             }
         });
 
+        //'take photo' button listener
+        addnewPhotosButton = (Button) rootView.findViewById(R.id.add_new_photos_event_button);
+        addnewPhotosButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                errorCheckingAndTakePhoto();
+                }
+
+        });
+
         return rootView;
     }
 
@@ -153,6 +175,36 @@ public class ViewPatientPhotoAlbumFragment extends Fragment {
         if (addNewPhotosExpandable.isOpened()) {
             addNewPhotosExpandable.hide();
         }
+    }
+
+    private void errorCheckingAndTakePhoto(){
+        String addPhotosSpinnerText = null;
+        if(photoAlbumTitleSpinner.getSelectedItemPosition() != 0) {
+            addPhotosSpinnerText = photoAlbumTitleSpinner.getSelectedItem().toString();
+        }
+        // Input checking to see if album title has been selected a not.
+        boolean isValidForm = true;
+        String errorMessage = getResources().getString(R.string.error_msg_field_required);
+
+        if(UtilsString.isEmpty(addPhotosSpinnerText)) {
+            photoAlbumTitleSpinner.setError(errorMessage);
+            isValidForm = false;
+        }
+
+        if (isValidForm){
+            dispatchTakePictureIntent();
+        }
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        File file = new File(path);
+        outputFileUri = Uri.fromFile(file);
+
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
     }
 
 }
