f493f998d4ca60e36f603cba7f12c17600209e56
teok0045 <heroes90>
2016-02-03 02:41:28 +0800

    Added a button intenting to link to 'assigned games activity' when patient's schedule contains "Android Game".

--- app/src/main/java/com/example/keith/fyp/views/adapters/EventArrayAdapter.java
+++ app/src/main/java/com/example/keith/fyp/views/adapters/EventArrayAdapter.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -74,7 +75,7 @@ public class EventArrayAdapter extends BaseAdapter {
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         View eventView = convertView;
-
+        Button gameButton;
         if (eventView == null) {
             LayoutInflater vi;
             vi = LayoutInflater.from(activity);
